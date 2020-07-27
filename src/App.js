eaf71beb9c3a0806c39e458d1e02d9c0f4e76b4b
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-07-26 22:10:25 -0700

    initial commit, implement regex to parse markdown headings

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    inputTxt: "",
+    htmlOut: ""
+  }
+
+  replacer = (inputTxt) => {
+    const inputArr = inputTxt.split('\n');
+    return inputArr.map(str => {
+      const headingMatch = str.match(/\n*#+\s+.+\n*/g);
+
+      if (headingMatch !== null) {
+        const matchStr = headingMatch[0];
+        const level = matchStr.match(/#+/)[0].length;
+        return `
+          <h${level}>
+            ${matchStr.replace(/#+/, "").trim()}
+          </h${level}>
+          `;
+      }
+      return str;
+    })
+
+    // const heading = match.replace(/\n*#+\s+.+\n*/g, (m) => {
+    //   const level = m.match(/#+/)[0].length;
+    //   return `
+    //     <h${level}>
+    //       ${m.replace(/#+/, "").trim()}
+    //     </h${level}>
+    //     `;
+    // });
+
+    // return heading;
+
+  }
+
+  handleChange = (e) => {
+    const inputTxt = e.target.value;
+    // const html = txt.replace(/(.*)/g, (match) => this.replacer(match))
+    const htmlOut = this.replacer(inputTxt).join("");
+
+    this.setState({
+      inputTxt,
+      htmlOut
+    })
+  }
+
+  render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            Markdown Notes
+          </header>
+          
+          <main className="main--container">
+
+            <div id="editor" className="editor--split main--split">
+              <div className="editor--inner">
+                <textarea
+                  className="editor--textarea"
+                  value={this.state.txtIn}
+                  onChange={(e) => this.handleChange(e)}></textarea>
+              </div>
+            </div>
+
+            <div id="preview" className="preview--split main--split">
+              <div
+                className="preview--inner"
+                dangerouslySetInnerHTML={{__html: this.state.htmlOut}} />
+            </div>
+
+          </main>
+        </div>
+      );
+  }
 }
 
 export default App;
