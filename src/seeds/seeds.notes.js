ad91ad365e5aade7499cddc19080fa096350c17d
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-09-05 13:20:22 -0700

    add mock markdown note data

---
+++ src/seeds/seeds.notes.js
@@ -0,0 +1,77 @@
+const NOTES = [
+  {
+    id: 1,
+    user_id: 1,
+    title: "SVGs in JSX",
+    public: false,
+    content:
+      "# SVGs in JSX \n - [SVGtoJSX Electron App](https://github.com/SaraVieira/svg-to-jsx-electron/) \n ```- [SVG Basic Shapes](https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Basic_Shapes)```",
+  },
+  {
+    id: 2,
+    user_id: 1,
+    title: "Static site generation with Hugo",
+    public: true,
+    content: "fd",
+  },
+  {
+    id: 3,
+    user_id: 1,
+    title: "Useful javascript array methods",
+    public: false,
+    content: "fd",
+  },
+  {
+    id: 4,
+    user_id: 1,
+    title: "HTTP status codes cheatesheet",
+    public: false,
+    content: "fd",
+  },
+  {
+    id: 5,
+    user_id: 1,
+    title: "Custom HTML Elements with Web Component",
+    public: false,
+    content: "fd",
+  },
+  {
+    id: 6,
+    user_id: 1,
+    title: "Intro to Web Assembly",
+    public: false,
+    content: "fd",
+  },
+  {
+    id: 7,
+    user_id: 1,
+    title: "Understanding Go Interfaces",
+    public: false,
+    content: "fd",
+  },
+  {
+    id: 8,
+    user_id: 1,
+    title: "HTTP services in Golang",
+    public: false,
+    content: "fd",
+  },
+  {
+    id: 9,
+    user_id: 1,
+    title: "Markdown Cheatsheet",
+    public: false,
+    content:
+      "# Markdown Cheatsheet \n - [Github Markdown Syntax](https://guides.github.com/pdfs/markdown-cheatsheet-online.pdf)",
+  },
+  {
+    id: 10,
+    user_id: 1,
+    title: "CSS Animations",
+    public: false,
+    content:
+      "# CSS Animations \n - Use @keyframes to control the intermediate steps in a CSS animation sequence \n - [MDN @keyframes ref](https://developer.mozilla.org/en-US/docs/Web/CSS/@keyframes)",
+  },
+];
+
+export default NOTES;
