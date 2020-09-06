f3b4008021721cefc17dd88360a8807fc526a0e0
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-09-06 16:29:54 -0700

    add to dashboard layout

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
-const Dashboard = () => (
-  <div>
-    <h1>Dashboard Page</h1>
+const Dashboard = (props) => (
+  <div className="main__dashboard">
+    <h1>Latest Notes</h1>
+    <div className="main__dashboard--inner">
+      {'123456'.split('').map((note, idx) => {
+          return (
+            <div 
+              className="main__dashboard--latest"
+              key={idx}>
+                <a href="/">{note}</a>
+            </div>
+          )
+        })
+      }
+    </div>
   </div>
 );
 
