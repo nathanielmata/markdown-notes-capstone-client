fa254edd59d444f714443b2f19977b6b71537397
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-09-06 16:28:47 -0700

    add toggleHiddenMenu() to context

--- src/MarkdownNotesContext.js
+++ src/MarkdownNotesContext.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const MarkdownNotesContext = React.createContext({
-    getNote: () => {},
+  toggleHiddenMenu: () => {},
+  getNote: () => {},
 });
 
 export default MarkdownNotesContext;
