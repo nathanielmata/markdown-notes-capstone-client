425d8e8c660014a42526d2c2b22f3b7eac0c2f6a
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-09-06 16:23:25 -0700

    add created_at and updated_at to seeds

--- src/seeds/seeds.notes.js
+++ src/seeds/seeds.notes.js
@@ -4,6 +4,8 @@ const NOTES = [
     user_id: 1,
     title: "SVGs in JSX",
     public: false,
+    created_at: '2020-01-13T16:28:32.615Z',
+    updated_at: '2020-01-13T17:28:32.615Z',
     content:
       "# SVGs in JSX \n - [SVGtoJSX Electron App](https://github.com/SaraVieira/svg-to-jsx-electron/) \n ```- [SVG Basic Shapes](https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Basic_Shapes)```",
   },
@@ -12,6 +14,8 @@ const NOTES = [
     user_id: 1,
     title: "Static site generation with Hugo",
     public: true,
+    created_at: '2020-01-14T16:28:32.615Z',
+    updated_at: '2020-01-14T17:28:32.615Z',
     content: "fd",
   },
   {
@@ -19,6 +23,8 @@ const NOTES = [
     user_id: 1,
     title: "Useful javascript array methods",
     public: false,
+    created_at: '2020-01-15T16:28:32.615Z',
+    updated_at: '2020-01-15T17:28:32.615Z',
     content: "fd",
   },
   {
@@ -26,6 +32,8 @@ const NOTES = [
     user_id: 1,
     title: "HTTP status codes cheatesheet",
     public: false,
+    created_at: '2020-01-16T16:28:32.615Z',
+    updated_at: '2020-01-16T17:28:32.615Z',
     content: "fd",
   },
   {
@@ -33,6 +41,8 @@ const NOTES = [
     user_id: 1,
     title: "Custom HTML Elements with Web Component",
     public: false,
+    created_at: '2020-01-17T16:28:32.615Z',
+    updated_at: '2020-01-17T17:28:32.615Z',
     content: "fd",
   },
   {
@@ -40,6 +50,8 @@ const NOTES = [
     user_id: 1,
     title: "Intro to Web Assembly",
     public: false,
+    created_at: '2020-01-18T16:28:32.615Z',
+    updated_at: '2020-01-18T17:28:32.615Z',
     content: "fd",
   },
   {
@@ -47,6 +59,8 @@ const NOTES = [
     user_id: 1,
     title: "Understanding Go Interfaces",
     public: false,
+    created_at: '2020-01-19T16:28:32.615Z',
+    updated_at: '2020-01-19T17:28:32.615Z',
     content: "fd",
   },
   {
@@ -54,6 +68,8 @@ const NOTES = [
     user_id: 1,
     title: "HTTP services in Golang",
     public: false,
+    created_at: '2020-01-20T16:28:32.615Z',
+    updated_at: '2020-01-20T17:28:32.615Z',
     content: "fd",
   },
   {
@@ -61,6 +77,8 @@ const NOTES = [
     user_id: 1,
     title: "Markdown Cheatsheet",
     public: false,
+    created_at: '2020-01-21T16:28:32.615Z',
+    updated_at: '2020-01-21T17:28:32.615Z',
     content:
       "# Markdown Cheatsheet \n - [Github Markdown Syntax](https://guides.github.com/pdfs/markdown-cheatsheet-online.pdf)",
   },
@@ -69,6 +87,8 @@ const NOTES = [
     user_id: 1,
     title: "CSS Animations",
     public: false,
+    created_at: '2020-01-22T16:28:32.615Z',
+    updated_at: '2020-01-22T17:28:32.615Z',
     content:
       "# CSS Animations \n - Use @keyframes to control the intermediate steps in a CSS animation sequence \n - [MDN @keyframes ref](https://developer.mozilla.org/en-US/docs/Web/CSS/@keyframes)",
   },
