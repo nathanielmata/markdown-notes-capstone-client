cef8949cd498930c0dad06a20693c6da560a2cac
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-09-07 00:53:19 -0700

    seed some users

---
+++ src/seeds/seeds.users.js
@@ -0,0 +1,25 @@
+const USERS = [
+  {
+    id: 1,
+    name: "Nathaniel Mata",
+    email: "nathaniel@thepixelparty.com",
+    created_at: '2020-01-13T16:28:32.615Z',
+    updated_at: '2020-01-13T17:28:32.615Z',
+  },
+  {
+    id: 2,
+    name: "Nathaniel Rose",
+    email: "nathaniel@ahdios.com",
+    created_at: '2020-01-13T16:28:32.615Z',
+    updated_at: '2020-01-13T17:28:32.615Z',
+  },
+  {
+    id: 3,
+    name: "Nathaniel Homer",
+    email: "nate@nathanielmata.com",
+    created_at: '2020-01-13T16:28:32.615Z',
+    updated_at: '2020-01-13T17:28:32.615Z',
+  },
+ ];
+
+export default USERS;
