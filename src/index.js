aec0633f25ce627fd3865f6bec876144cf6509ae
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-09-07 09:36:54 -0700

    add react-snapshot to generate static html pages for wireframes, dynamic routes not created

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import './index.css';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+render(
+  <App />,
   document.getElementById('root')
 );
 
