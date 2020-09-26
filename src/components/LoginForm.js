58325f8cd2b71135b18fda853e66cec092a062e2
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-09-26 08:36:15 -0700

    remove saveAuthToken as it executes when AuthApiService postLogin method is called

---
+++ src/components/LoginForm.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import AuthApiService from "../services/auth-api-service";
+
+export default class LoginForm extends Component {
+  static defaultProps = {
+    onLoginSuccess: () => {},
+  };
+
+  state = { error: null };
+
+  handleSubmitJwtAuth = (ev) => {
+    ev.preventDefault();
+    this.setState({ error: null });
+    const { user_name, password } = ev.target;
+
+    AuthApiService.postLogin({
+      user_name: user_name.value,
+      password: password.value,
+    })
+      .then((res) => {
+        user_name.value = "";
+        password.value = "";
+        this.props.onLoginSuccess();
+      })
+      .catch((res) => {
+        this.setState({ error: res.error });
+      });
+  };
+
+  render() {
+    const { error } = this.state;
+    return (
+      <form className="form__login" onSubmit={this.handleSubmitJwtAuth}>
+        <div role="alert">{error && <p className="red">{error}</p>}</div>
+        <div className="user_name">
+          <label htmlFor="user_name">User name</label>
+          <input required name="user_name" id="user_name"></input>
+        </div>
+        <div className="password">
+          <label htmlFor="password">Password</label>
+          <input
+            required
+            name="password"
+            type="password"
+            id="password"
+          ></input>
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    );
+  }
+}
