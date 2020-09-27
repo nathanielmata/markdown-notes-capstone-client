12b8fd9dc48c25258e2c9017c5b8d19b0ddf0cad
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-09-27 12:40:07 -0700

    add API_ENDPOINT env var for local developement

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://infinite-sea-85899.herokuapp.com/api',
+  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'https://infinite-sea-85899.herokuapp.com/api',
   TOKEN_KEY: 'markdown-notes-client-auth-token',
 }
