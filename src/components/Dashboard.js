67e33b0392bb24be2ffae3c9cceb84d5cfccd74e
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-09-27 13:14:50 -0700

    move timeAgo method into utils folder

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,41 +1,10 @@
 import React from "react";
+import timeAgo from "../utils/timeAgo";
 import NoteListContext from "../context/NoteListContext";
 
 class Dashboard extends React.Component {
   static contextType = NoteListContext;
-
-  timeUnits = () => {
-    // time units (minute, hour, day) in milliseconds
-    const unitMs = {
-      m: 1000*60,
-      h: 1000*60*60,
-      d: 1000*60*60*24,
-    }
-
-    // return array of objects containing
-    // r: time range validator
-    // d: time unit in milliseconds
-    // s: time suffix
-    return [
-      {r: (ms) => ms < unitMs.h, d: unitMs.m, s: " min ago"},
-      {r: (ms) => ms > unitMs.h && ms < unitMs.d, d: unitMs.h, s: " hours ago"},
-      {r: (ms) => ms > unitMs.d, d: unitMs.d, s: " days ago"},
-    ];
-  }
-
-  timeAgo = (date) => {
-    const ms = (Date.now() - Date.parse(date));
-    let time_ago = "";
-    this.timeUnits().forEach(timestamp => {
-      if (timestamp.r(ms)) {
-        let timeUnits = Math.round(ms/timestamp.d).toString();
-        let suffix = timestamp.s;
-        time_ago = timeUnits + suffix;
-      }
-    })
-    return time_ago;
-  }
-
+  
   render() {
     const { notes } = this.context;
     console.log(notes);
@@ -49,7 +18,7 @@ class Dashboard extends React.Component {
                 <a href={`/note/${note.id}`}>
                   <div className="main__dashboard--card-name">{note.title}</div>
                   <div className="main__dashboard--card-date">
-                    Edited {this.timeAgo(note.updated_at ?? note.created_at)}
+                    Edited {timeAgo.parse(note.updated_at ?? note.created_at)}
                   </div>
                 </a>
               </div>
---
+++ src/utils/timeAgo.js
@@ -0,0 +1,39 @@
+const timeAgo = (() => {
+
+  function timeUnits() {
+    // time units (minute, hour, day) in milliseconds
+    const unitMs = {
+      m: 1000*60,
+      h: 1000*60*60,
+      d: 1000*60*60*24,
+    }
+
+    // return array of objects containing
+    // r: time range validator
+    // d: time unit in milliseconds
+    // s: time suffix
+    return [
+      {r: (ms) => ms < unitMs.h, d: unitMs.m, s: " min ago"},
+      {r: (ms) => ms > unitMs.h && ms < unitMs.d, d: unitMs.h, s: " hours ago"},
+      {r: (ms) => ms > unitMs.d, d: unitMs.d, s: " days ago"},
+    ];
+  };
+
+  return {  
+    parse: (date) => {
+      const ms = (Date.now() - Date.parse(date));
+      let time_ago = "";
+      timeUnits().forEach(timestamp => {
+        if (timestamp.r(ms)) {
+          let timeUnits = Math.round(ms/timestamp.d).toString();
+          let suffix = timestamp.s;
+          time_ago = timeUnits + suffix;
+        }
+      })
+      return time_ago;
+    },
+  };
+
+})();
+
+export default timeAgo;
