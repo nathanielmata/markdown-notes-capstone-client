dee9715aed96a4aded5e05ae092c26e39551dfcd
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-11-27 11:26:12 -0700

    set userCtx if we can get a jwt token from the server

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -11,7 +11,7 @@ export default function PrivateRoute({ component, ...props }) {
     // read the auth token to set the user in context
     const user = TokenService.readJwtToken();
     // use a conditional here to prevent an infinite loop
-    if (!user || userCtx.user.user_id !== user.user_id) {
+    if (user && userCtx.user.user_id !== user.user_id) {
       userCtx.setUser(user);
     }
   }, [userCtx]);
